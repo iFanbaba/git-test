377709261112fcb2dc77a79b6216d3158e942fd4
MYF <673073560@qq.com>
2022-01-07 16:48:41 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @create 2022/1/7
+ * @create 16:43
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git!!!");
+    }
+}
